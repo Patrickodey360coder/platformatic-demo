d840f8ff707c79230bd438c70f957d639c7f5f4e
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-29 07:11:14 -0500

    server config

--- backend/services/job-board/migrations/001.do.sql
+++ backend/services/job-board/migrations/001.do.sql
@@ -2,6 +2,6 @@
   -- Add SQL in this file to create the database tables for your API
   CREATE TABLE IF NOT EXISTS movies (
     id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL
+    title VARCHAR(255) NOT NULL
   );
   
